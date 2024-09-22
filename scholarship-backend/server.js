b26ec4c07e0871f229d35d37b24d784bba54e47c
Ramana Gowrishetty <ramana.03shetty@gmail.com>
2024-09-22 20:16:36 +0530

    Database connection

--- scholarship-backend/server.js
+++ scholarship-backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 require("dotenv").config();
 
+const connection = require("./configuration/db.js");
+
 const app = express();
 
 app.get("/", (req, res) => {
   console.log("sdfkasdjkasdjlkasjdasjf");
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`App is listening on port ${process.env.PORT || 8080}`);
+connection.connect((err) => {
+  if (err) {
+    console.log("user: ", process.env.MYSQL_USER);
+    console.log(err.message);
+    return;
+  }
+  console.log("Database connection successfull");
+  app.listen(process.env.PORT || 8080, () => {
+    console.log(`App is listening on port ${process.env.PORT || 8080}`);
+  });
 });
